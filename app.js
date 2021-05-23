46f829af26780055e7d28139b670b3a9c023d53d
Frank <fauletto@gmail.com>
2021-05-23 16:13:20 -0400

    Added some basic layout features for 1RM entry. Added variables containing an array of lifts for T1, T2, and T3 lifts.

---
+++ app.js
@@ -0,0 +1,8 @@
+// Variables to store array of lifts to be randomized
+const upperBodyLifts = ["Bench Press", "Incline Bench Press", "Overhead Press", "Push Press", "Barbell Row"];
+const lowerBodyLifts = ["Back Squat", "Front Squat", "Conventional Deadlift", "Sumo Deadlift", "Hack Squat"];
+const accessoryLifts = ["Pull Up", "Chin Up", "Dip", "Dumbbell Hammer Curl", "EZ Bar/Barbell Curl", "Triceps Pushdown", "Skullcrusher", "Incline Dumbbell Bench Press", "Flat Dumbbell Bench Press", "Lateral Raise", "Dumbbell Overhead Press", "Dumbbell SLDL", "Dumbbell RDL", "Goblet Squat", "Leg Press", "Leg Extension"];
+
+const getRandNumber = (n) =>{
+  return Math.floor(Math.random() * n);
+}
