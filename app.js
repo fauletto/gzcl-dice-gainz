40a5b3daa3ea95b6f8406481d08d3d3dfbccf089
Frank <fauletto@gmail.com>
2021-05-27 20:19:15 -0400

    Added logic for randomizing reps. Added a table to display lifts, reps, and weights.

--- app.js
+++ app.js
@@ -6,6 +6,15 @@ const squatMaxInput = document.querySelector('#inputSquatMax');
 const benchMaxInput = document.querySelector('#inputBenchMax');
 const deadliftMaxInput = document.querySelector('#inputDeadliftMax');
 const overheadMaxInput = document.querySelector('#inputOverheadMax');
+const t1LiftDisplay = document.querySelector('#t1LiftDisplay');
+const t2LiftDisplay = document.querySelector('#t2LiftDisplay');
+const t3ALiftDisplay = document.querySelector('#t3ALiftDisplay');
+const t3BLiftDisplay = document.querySelector('#t3BLiftDisplay');
+const t3CLiftDisplay = document.querySelector('#t3CLiftDisplay');
+const t1FirstRepDisplay = document.querySelector('#t1FirstRepDisplay');
+const t2FirstRepDisplay = document.querySelector('#t2FirstRepDisplay');
+const t1FollowUpRepDisplay = document.querySelector('#t1FollowUpRepDisplay');
+const t2FollowUpRepDisplay = document.querySelector('#t2FollowUpRepDisplay');
 const getRandNumber = (n) =>{
   return Math.floor(Math.random() * n);
 }
@@ -18,6 +27,8 @@ let squatMax;
 let benchMax;
 let deadliftMax;
 let overheadMax;
+let t1Reps;
+let t2Reps;
 
 const getOneRepMax = (weightInput) =>{
   return weightInput.value;
@@ -36,15 +47,22 @@ const getLift = (liftInput) =>{
 }
 
 const getReps = () =>{
-  
+  t1Reps = getRandNumber(6-3) + 3;
+  t2Reps = getRandNumber(10-6) + 6;
+  t1FirstRepDisplay.innerText = `${t1Reps}`;
+  t2FirstRepDisplay.innerText = `${t2Reps}`;
+  t1FollowUpRepDisplay.innerText = `${t1Reps} sets of 5`;
+  t2FollowUpRepDisplay.innerText = `4 sets of ${t2Reps / 2}`;
 }
 
 const upperRoutine = () =>{
-  console.log("Upper day!");
+  console.log("You've selected upper body lifts only!");
+  return upperBodyLifts[getRandNumber(upperBodyLifts.length)];
 }
 
 const lowerRoutine = () =>{
-  console.log("Lower day!");
+  console.log("You've selected lower body lifts only!");
+  return lowerBodyLifts[getRandNumber(upperBodyLifts.length)];
 }
 
 const fullRoutine = () =>{
@@ -54,6 +72,7 @@ const fullRoutine = () =>{
 const getRoutine = () =>{
   if(upperSelect.checked){
     upperRoutine();
+    getReps();
   } else if (lowerSelect.checked) {
     lowerRoutine();
   } else if (fullSelect.checked){
@@ -62,6 +81,7 @@ const getRoutine = () =>{
 }
 
 rollDiceBtn.addEventListener('click', function(){
-  getMaxes();
+  // getMaxes();
   getRoutine();
+  getReps();
 })
