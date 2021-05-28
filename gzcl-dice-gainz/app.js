b8369f1fe91890448b763d71e326f4be767a25af
Frank Auletto <frankauletto@Franks-MacBook-Air.local>
2021-05-28 14:03:51 -0400

    Added second table to separate T3 lifts from T1 and T2 lifts.

--- gzcl-dice-gainz/app.js
+++ gzcl-dice-gainz/app.js
@@ -1,7 +1,6 @@
 // Variables to store array of lifts to be randomized
 const upperBodyLifts = ["Bench Press", "Incline Bench Press", "Overhead Press", "Push Press", "Barbell Row"];
 const lowerBodyLifts = ["Back Squat", "Front Squat", "Conventional Deadlift", "Sumo Deadlift", "Hack Squat"];
-const accessoryLifts = ["Pull Up", "Chin Up", "Dip", "Dumbbell Hammer Curl", "EZ Bar/Barbell Curl", "Triceps Pushdown", "Skullcrusher", "Incline Dumbbell Bench Press", "Flat Dumbbell Bench Press", "Lateral Raise", "Dumbbell Overhead Press", "Dumbbell SLDL", "Dumbbell RDL", "Goblet Squat", "Leg Press", "Leg Extension"];
 const squatMaxInput = document.querySelector('#inputSquatMax');
 const benchMaxInput = document.querySelector('#inputBenchMax');
 const deadliftMaxInput = document.querySelector('#inputDeadliftMax');
